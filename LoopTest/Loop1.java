6e8e57779ee0ff6290d761e3f63a6c63af714d13
OtonielC <800494@seq.org>
2019-09-10 09:57:38 -0700

    9/10 (this was made afew days ago)

---
+++ LoopTest/Loop1.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Loop1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Loop1
+{
+
+    public static void main(){
+        for(int i=12; i>6; i=i-3){
+            i = 12-i;
+            System.out.println(i);
+        }
+
+    }
+}
