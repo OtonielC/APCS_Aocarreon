527cebdb058355af30f56233e778521755ddefb5
OtonielC <800494@seq.org>
2019-09-10 10:30:25 -0700

    910 in class for arrays

---
+++ ArrayLab 910/ArrayClass.java
@@ -0,0 +1,21 @@
+import static java.lang.System.*;
+/**
+ *
+ * @author (Oats)
+ * @version (9/10)
+ */
+public class ArrayClass
+{
+  int[] nums = new int[100];
+    public ArrayClass()
+    {
+       
+    }
+    public void loadArray(){
+            
+        }
+  public void loadArray(){
+            
+        }
+        
+}
