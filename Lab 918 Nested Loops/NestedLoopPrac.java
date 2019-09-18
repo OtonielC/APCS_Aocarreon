3bf81ee7b9436733fea2a934ec5fd68ddc9058cc
OtonielC <800494@seq.org>
2019-09-18 10:21:36 -0700

    9/18 Sch

---
+++ Lab 918 Nested Loops/NestedLoopPrac.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class NestedLoopPrac here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class NestedLoopPrac
+{
+    public NestedLoopPrac()
+    {
+        
+    }
+    
+    public int triangle(){
+        for(var r = 0; r < 5; r++){
+            for(var c = 0; c < r; c++){
+                
+            }
+        }
+    }
+}
