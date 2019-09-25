9561c1ef4c7e68c50820e433046e341d94ca9ba6
OtonielC <800494@seq.org>
2019-09-24 22:47:21 -0700

    9/24 Make Up Work At Home
    I worked on nested loops and some thinking on the scanner test lab

--- Lab 918 Nested Loops/NestedLoopPrac.java
+++ Lab 918 Nested Loops/NestedLoopPrac.java
@@ -12,11 +12,29 @@ public class NestedLoopPrac
         
     }
     
-    public int triangle(){
-        for(var r = 0; r < 5; r++){
+    public static void halfPyramid(){
+        for(var r = 0; r <= 5; r++){
             for(var c = 0; c < r; c++){
-                
+                System.out.print(r);
             }
+            System.out.println();
         }
+    }
+    public static void square(){
+        for(var i = 5; i <= 10; i++){
+            for(var j = 0; j < i; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    public static void universal(){
+        for(var r = 0; r <= 5; r++){
+            for(var c = 0; c < r; c++){
+                System.out.print(r);
+            }
+            System.out.println();
+        }
+
     }
 }
--- ScannerTest/Person.java
+++ ScannerTest/Person.java
@@ -11,16 +11,18 @@ public class Person
     public String mname;
     public String lname;
     public void parseName(String fullName){
-        lname = fullName.substring(0,fullName.indexOf(","));
+         lname = fullName.substring(0,fullName.indexOf(","));
         fname = fullName.substring(fullName.indexOf(","), fullName.indexOf(" "));
-    }
+     }
 
     public Person(String fullName){
         //System.out.println("Your name is " + lname + ", " + fname + " " + mname);
         System.out.println(fname);
         System.out.println(mname);
         System.out.println(lname);
-        
-        //if(!fullName)
-     } 
+
+        //if(){
+            
+        //}
+    }
 }
