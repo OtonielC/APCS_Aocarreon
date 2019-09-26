2fda88785b194240fc23b6cfc46992f8019d4c43
OtonielC <800494@seq.org>
2019-09-26 10:27:17 -0700

    9/26 School

--- ScannerTest/Person.java
+++ ScannerTest/Person.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 /**
  * Runner for Scanner Test
  *
@@ -10,19 +10,40 @@ public class Person
     public String fname;
     public String mname;
     public String lname;
-    public void parseName(String fullName){
-         lname = fullName.substring(0,fullName.indexOf(","));
-        fname = fullName.substring(fullName.indexOf(","), fullName.indexOf(" "));
-     }
-
     public Person(String fullName){
-        //System.out.println("Your name is " + lname + ", " + fname + " " + mname);
-        System.out.println(fname);
-        System.out.println(mname);
-        System.out.println(lname);
-
-        //if(){
-            
-        //}
+        if(fullName.indexOf(",") != -1){
+            int s1 = fullName.indexOf(",");
+            lname = fullName.substring(0, s1);
+            if(fullName.indexOf(" ") != fullName.lastIndexOf(" ")){
+                int s2 = fullName.indexOf(" ");
+                int s3 = fullName.lastIndexOf(" ");
+                fname = fullName.substring(s2, s3);
+                int s4 = fullName.lastIndexOf(" ");
+                mname = fullName.substring(s4 +1);
+            }
+            else{
+                int s2 = fullName.indexOf(" ");
+                int s3 = fullName.lastIndexOf(" ");
+                fname = fullName.substring(s2, s3);
+            }
+            System.out.print(fname + " " + mname + " " + lname + ".");
+        }
+        if(fullName.indexOf(",") == -1){
+            int s1 = fullName.indexOf(" ");
+            fname = fullName.substring(0,s1);
+            if(fullName.indexOf(" ") != fullName.lastIndexOf(" ")){
+                int s2 = fullName.lastIndexOf(" ");
+                mname = fullName.substring(s1, s2);
+                int s3 = fullName.lastIndexOf(" ");
+                lname = fullName.substring(s3);
+                System.out.print(fname + " " + mname + " " + lname + ".");
+            }
+            else{
+                int s2 = fullName.lastIndexOf(" ");
+                lname = fullName.substring(s2);   
+                System.out.print(fname + " " + lname + ".");
+            }
+        }
+  
     }
 }
