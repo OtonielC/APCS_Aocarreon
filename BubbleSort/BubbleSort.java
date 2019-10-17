5e7dfcfe982b02a15e0da72790b45e34f354cedf
OtonielC <800494@seq.org>
2019-10-17 10:30:14 -0700

    10/17
    Bubble Sort

---
+++ BubbleSort/BubbleSort.java
@@ -0,0 +1,21 @@
+import java.util.*;
+/**
+ * a type of sort
+ *
+ * @author (Oats)
+ * @version (10/17)
+ */
+public class BubbleSort
+{
+    private ArrayList<Integer> list = new ArrayList<Integer>();
+    
+    public BubbleSort(){
+        
+    }
+    
+    public void loadArray(){
+        for(int i = 0; i < list.size(); i++){
+            
+        }
+    }
+}
--- MMM-ArrayList/MMM.java
+++ MMM-ArrayList/MMM.java
@@ -1,11 +1,61 @@
-
+import java.util.*;
 /**
  * Write a description of class MMM here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class MMM
-{
+public class MMM{
+    private ArrayList<Integer> list = new ArrayList<Integer>();
+    private double mean;
+    private double median;
+    private int[] mode = {-1, 1};
+
+    public void MMM(){
+        loadList(100);
+        printList();
+        findMean();
+        getSum(list);
+        findMedian();
+    }
+
+    public void loadList(int n){
+        for(int i = 0; i < n; i++){
+            int rnd = (int)(Math.random()*100) + 1;
+            list.add(rnd);
+        }
+    }
+
+    public void printList(){
+        for(int i = 0; i < list.size(); i++){
+            System.out.print(list.get(i) + ", ");
+        }
+        System.out.println();
+    }
+
+    public double findMean(){
+        double sum = getSum(list);
+
+        return (double)(sum/list.size());
+    }
+
+    public Integer getSum(ArrayList<Integer> numList){
+        int sum = 0;
+
+        for(int i = 0; i < numList.size(); i++){
+            sum += numList.get(i); 
+        }
+        return sum;
+    }
+
+    public double findMedian(){
+        double sum = 0;
+        if(list.size() % 2 == 0){
+            sum = list.get(list.size()/2 + list.size()/2 -1)/2;
+        }else{
+            sum = list.get(list.size()/2);
+        }
+        return sum;
+    }
     
-}
+  }
