1ec80299c4f95bb924e477256f8246c7b35cd492
OtonielC <800494@seq.org>
2019-11-04 11:00:32 -0800

    11/4

--- StringChecker/StringCheck.java
+++ StringChecker/StringCheck.java
@@ -7,6 +7,16 @@ import java.util.*;
  */
 public class StringCheck
 {   
+    private ArrayList<String> wordList = new ArrayList<String>();
+    private String[] tempList = {"cat", "catastrophe", "caterpillar", "dog"};
+    
+    public StringCheck(){
+        wordChecker(tempList, "cat");
+        for(int i = 0; i < tempList.length; i++){
+            System.out.print(tempList[i]);
+        }
+    }
+    
     public static void main(String[] args){
         ArrayList<String> strList = new ArrayList<String>(10);
         
@@ -14,17 +24,29 @@ public class StringCheck
         strList.add("caterpillar");
         strList.add("catastrophe");
         strList.add("dog");
-        
-        Scanner kb = new Scanner(System.in);
-        String inputStr = "";
-        while(!inputStr.equals("quit")){
-            System.out.println("Enter a key or type \"quit\" to quit");
-            inputStr = kb.nextLine();
-            System.out.println(inputStr);
+    }
+    
+    public String[] wordChecker(ArrayList<String> str, String key){
+        String[] tempList = new String[10];
+        for(int i = 0; i < str.size(); i++){
+            if(!str.get(i).equals(null)){
+                if(str.get(i).indexOf("cat") != -1){
+                    tempList[i].equals(key);
+                }
+            }
         }
-        
-        
-        
-        System.out.println(strList);
+        return tempList;
+    }
+    
+    public ArrayList<String> wordChecker(String[] str, String key){
+        ArrayList<String> tempList = new ArrayList<String>();
+        for(int i = 0; i < str.length; i++){
+            if(str[i] != null){
+                if(str[i].indexOf(key) != -1){
+                    tempList.add(str[i]);
+                }
+            }
+        }
+        return tempList;
     }
 }
