d482eef4bf7c78e86b41e9de0a6786d9a0c7be0a
OtonielC <800494@seq.org>
2019-11-04 11:29:21 -0800

    10/4 second push

--- Lab 918 Nested Loops/NestedLoopPrac.java
+++ Lab 918 Nested Loops/NestedLoopPrac.java
@@ -9,32 +9,54 @@ public class NestedLoopPrac
 {
     public NestedLoopPrac()
     {
-        
+        halfPyramid();
+        square();
+        universal();
+        e();
     }
     
-    public static void halfPyramid(){
-        for(var r = 0; r <= 5; r++){
-            for(var c = 0; c < r; c++){
-                System.out.print(r);
-            }
-            System.out.println();
-        }
-    }
-    public static void square(){
-        for(var i = 5; i <= 10; i++){
-            for(var j = 0; j < i; j++){
+    public void halfPyramid(){
+      int rows = 5;
+      for(int i = 1; i <= rows; ++i)
+      {
+          for(int j = 1; j <= i; ++j)
+          {
+             System.out.print(j + " ");
+          }
+          System.out.println("");
+      }
+   }
+    
+    public void square(){
+        for(int i = 5; i <= 10; i++){
+            for(int j = 0; j < i; j++){
                 System.out.print(i);
             }
             System.out.println();
         }
     }
-    public static void universal(){
-        for(var r = 0; r <= 5; r++){
-            for(var c = 0; c < r; c++){
+    public void universal(){
+        for(int r = 0; r <= 5; r++){
+            for(int c = 0; c < r; c++){
                 System.out.print(r);
             }
             System.out.println();
         }
 
+    }
+    
+    public void e(){
+       for(int i = 5; i > -5; i--)
+      {
+         for(int j = 0; j < i; j++)
+         {
+            System.out.print(" ");
+         }
+         for(int j = 0; j >= i; j--)
+         {
+            System.out.print(" ");
+         }
+         System.out.println("*");
+      }
     }
 }
